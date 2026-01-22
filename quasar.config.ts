0263872c4899eddf990b94ceb94d7e143ca15967
Tom <114468367+Tom59sly@users.noreply.github.com>
2026-01-22 14:34:40 +0100

    Fix formatting of ssr configuration in quasar.config.ts

--- quasar.config.ts
+++ quasar.config.ts
@@ -122,7 +122,7 @@ export default defineConfig((/* ctx */) => {
     // },
 
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
-    ssr: false//{
+    ssr: false,//{
       //prodPort: 3000, // The default port that the production server should use
       // (gets superseded if process.env.PORT is specified at runtime)
 
